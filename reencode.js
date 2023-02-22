e81bec6baf642612372e01afbc176600e6641914
Arthur Laquièze <arthur.laquieze@gmail.com>
2023-02-22 10:49:10 +0100

    working reencode

--- reencode.js
+++ reencode.js
@@ -1,7 +1,8 @@
 const torrentStream = require("torrent-stream");
-const hbjs = require("handbrake-js");
+// const hbjs = require("handbrake-js");
 const ftp = require("basic-ftp");
 const fs = require("fs");
+const { spawn } = require("child_process");
 
 // load ftp credentials from the config file
 const ftpConfig = require("./ftp_credentials.json");
@@ -9,20 +10,18 @@ const ftpConfig = require("./ftp_credentials.json");
 // load torrent from file as a buffer
 const torrentFile = fs.readFileSync("./torrentfile.torrent");
 
-const torrentUrl = "http://example.com/torrentfile.torrent";
-
 const ftpPath = "/files/Anime/southpark";
 const tempDir = "./temp/";
-// create temp dir
-fs.mkdir(tempDir, { recursive: true }, (err) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log(`Temp dir ${tempDir} created successfully.`);
-  }
-});
+
+const presetFilePath = "south_park_best.json";
+
+// create temp dir if not exists
+if (!fs.existsSync(tempDir)) {
+  fs.mkdirSync(tempDir);
+}
 
 const engine = torrentStream(torrentFile);
+
 let index = 0;
 
 engine.on("ready", () => {
@@ -48,57 +47,85 @@ engine.on("ready", () => {
       output.on("finish", () => {
         console.log("File downloaded successfully. Starting reencoding...");
 
-        hbjs
-          .spawn({
-            input: temporaryPath,
-            output: `Reencoded_${tempDir}${file.name}`,
-            // preset: "south_park_best",
-            "preset-import-file": "south_park_best.json",
-          })
-          .on("error", (err) => {
-            console.error(err);
+        const inputPath = temporaryPath;
+        const outputPath = `${tempDir}reencoded_${file.name}`;
+
+        const handbrake = spawn("HandBrakeCLI", [
+          "-i",
+          inputPath,
+          "-o",
+          outputPath,
+          "--preset-import-file",
+          presetFilePath,
+          // `file:${presetFilePath}`,
+        ]);
+
+        handbrake.stdout.on("data", (data) => {
+          console.log(`HandBrakeCLI stdout: ${data}`);
+        });
+        handbrake.stderr.on("data", (data) => {
+          console.error(`HandBrakeCLI stderr: ${data}`);
+        });
+        handbrake.on("close", (code) => {
+          if (code !== 0) {
+            console.error(`HandBrakeCLI process exited with code ${code}`);
             // nextFile();
-          })
-          .on("complete", () => {
-            console.log("Reencoding complete.");
-            // ftpClient
-            //   .access(ftpConfig)
-            //   .then(() => {
-            //     return ftpClient.uploadFrom(
-            //       `${tempDir}${file.name}.mp4`,
-            //       `${ftpPath}${file.name}.mp4`
-            //     );
-            //   })
-            //   .then(() => {
-            //     console.log(`File ${file.name} uploaded successfully.`);
-            //     fs.unlink(temporaryPath, (err) => {
-            //       if (err) {
-            //         console.error(err);
-            //       } else {
-            //         console.log(
-            //           `Temporary file ${temporaryPath} deleted successfully.`
-            //         );
-            //       }
-            //     });
-            //     fs.unlink(`${tempDir}${file.name}.mp4`, (err) => {
-            //       if (err) {
-            //         console.error(err);
-            //       } else {
-            //         console.log(
-            //           `Temporary file ${tempDir}${file.name}.mp4 deleted successfully.`
-            //         );
-            //       }
-            //     });
-            //     nextFile();
-            //   })
-            //   .catch((err) => {
-            //     console.error(err);
-            //     nextFile();
-            //   })
-            //   .finally(() => {
-            //     ftpClient.close();
-            //   });
-          });
+          } else {
+            console.log(`HandBrakeCLI process finished successfully.`);
+          }
+        });
+
+        // hbjs
+        //   .spawn({
+        //     input: temporaryPath,
+        //     output: `Reencoded_${tempDir}${file.name}`,
+        //     // preset: "south_park_best",
+        //     "preset-import-file": "south_park_best.json",
+        //   })
+        //   .on("error", (err) => {
+        //     console.error(err);
+        //     // nextFile();
+        //   })
+        //   .on("complete", () => {
+        //     console.log("Reencoding complete.");
+        // ftpClient
+        //   .access(ftpConfig)
+        //   .then(() => {
+        //     return ftpClient.uploadFrom(
+        //       `${tempDir}${file.name}.mp4`,
+        //       `${ftpPath}${file.name}.mp4`
+        //     );
+        //   })
+        //   .then(() => {
+        //     console.log(`File ${file.name} uploaded successfully.`);
+        //     fs.unlink(temporaryPath, (err) => {
+        //       if (err) {
+        //         console.error(err);
+        //       } else {
+        //         console.log(
+        //           `Temporary file ${temporaryPath} deleted successfully.`
+        //         );
+        //       }
+        //     });
+        //     fs.unlink(`${tempDir}${file.name}.mp4`, (err) => {
+        //       if (err) {
+        //         console.error(err);
+        //       } else {
+        //         console.log(
+        //           `Temporary file ${tempDir}${file.name}.mp4 deleted successfully.`
+        //         );
+        //       }
+        //     });
+        //     nextFile();
+        //   })
+        //   .catch((err) => {
+        //     console.error(err);
+        //     nextFile();
+        //   })
+        //   .finally(() => {
+        //     ftpClient.close();
+        //   });
+        // });
       });
     }
   };
